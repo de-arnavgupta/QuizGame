7ff8c0958fb011ec31b773160415d62c40da6bdc
noir <noir@odin.com>
2024-08-17 21:41:07 +0530

    added background css and array for questions in java

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const quizData = [
+  {
+    question: "Which planet is known as the Red Planet?",
+    choices: ["Venus", "Mars", "Jupiter", "Saturn"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    choices: ["Earth", "Mars", "Jupiter", "Saturn"],
+    correctAnswer: 2
+  },
+  {
+    question: "How many planets are in our solar system?",
+    choices: ["7", "8", "9", "10"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the name of Earth's natural satellite?",
+    choices: ["Sun", "Moon", "Mars", "Venus"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which galaxy is the Milky Way's nearest neighbor?",
+    choices: ["Andromeda", "Triangulum", "Centaurus A", "Messier 87"],
+    correctAnswer: 0
+  }
+];
